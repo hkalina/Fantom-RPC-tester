4cb58043291078f1128c8c2a14ae4842aeb92c84
Jan Kalina <jan.kalina@fantom.foundation>
2022-06-16 10:49:31 +0200

    Retry block validation when RPC request fails

--- verifier/movements.go
+++ verifier/movements.go
@@ -6,14 +6,15 @@ import (
 	"math/big"
 )
 
-type BlockMovements struct {
+// BalancesChanges stores changes of balances on individual account during a block
+type BalancesChanges struct {
 	Map map[common.Address]*big.Int
 	Addresses []common.Address // for deterministic Map iterating
 }
 
-func (bm *BlockMovements) Add(address common.Address, amount *big.Int) {
+func (bm *BalancesChanges) Add(address common.Address, amount *big.Int) {
 	if address == (common.Address{}) || address == common.HexToAddress("0xFC00FACE00000000000000000000000000000000") {
-		return // skip validation for zero address
+		return // skip validation for zero/SFC address
 	}
 	item, exists := bm.Map[address]
 	if exists {
@@ -24,8 +25,9 @@ func (bm *BlockMovements) Add(address common.Address, amount *big.Int) {
 	}
 }
 
-func TxsIntoBlockMovements(txs []rpctypes.ExternalTx) *BlockMovements {
-	movements := new(BlockMovements)
+// AggregateTxsIntoBalancesChanges aggregates internal txs to one balance change per account
+func AggregateTxsIntoBalancesChanges(txs []rpctypes.ExternalTx) *BalancesChanges {
+	movements := new(BalancesChanges)
 	movements.Map = make(map[common.Address]*big.Int)
 	for _, tx := range txs {
 		for _, itx := range tx.InternalTxs {
--- verifier/verifier.go
+++ verifier/verifier.go
@@ -2,10 +2,12 @@ package verifier
 
 import (
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/hkalina/fantom-rpc-tester/client"
 	"github.com/hkalina/fantom-rpc-tester/rpctypes"
 	"log"
 	"math/big"
+	"strings"
 )
 
 type Verifier struct {
@@ -25,12 +27,23 @@ func NewVerifier(id int, debug bool) *Verifier {
 func (v *Verifier) VerifyRange(startBlock int64, endBlock int64, ftm *client.FtmBridge) {
 	v.printf("Started on range %d-%d\n", startBlock, endBlock)
 	for i := startBlock; i <= endBlock; i++ {
-		err := v.VerifyBlock(i, ftm)
+		err := v.VerifyBlockRepeatedly(i, ftm)
 		if err != nil {
 			log.Fatalf(v.prefix+ "VerifyBlock %d failed: %s", i, err)
 		}
 	}
 	v.printf("Finished successfully")
+}
+
+func (v *Verifier) VerifyBlockRepeatedly(blockNum int64, ftm *client.FtmBridge) (err error) {
+	for attempt := 1; attempt <= 5; attempt++ {
+		err = v.VerifyBlock(blockNum, ftm)
+		if err == nil || strings.HasPrefix(err.Error(), "unexpected balance") {
+			return nil
+		}
+		v.printf("VerifyBlock(%d) failed (attempt %d): %s\n", blockNum, attempt, err)
+	}
+	return err
 }
 
 func (v *Verifier) VerifyBlock(blockNum int64, ftm *client.FtmBridge) error {
@@ -46,22 +59,29 @@ func (v *Verifier) VerifyBlock(blockNum int64, ftm *client.FtmBridge) error {
 	if v.debug {
 		v.printTxs(txs)
 	}
-	movements := TxsIntoBlockMovements(txs)
+	balancesChanges := AggregateTxsIntoBalancesChanges(txs)
 	if v.debug {
-		v.printMovements(movements)
+		v.printBalancesChanges(balancesChanges)
 	}
-	for _, address := range movements.Addresses {
-		amount := movements.Map[address]
+	newBalances := make(map[common.Address]*big.Int, 50)
+	for _, address := range balancesChanges.Addresses {
+		balanceChange := balancesChanges.Map[address]
+
+		// get the account balance at the beginning of the block
 		oldBalance, err := v.cache.GetBalanceFromCacheOrLoad(address, prevBlockNum, ftm.GetBalance)
 		if err != nil {
 			return fmt.Errorf("unable to get balance for block %s: %s", prevBlockNum.String(), err)
 		}
+
+		// get the account balance at the end of the block
 		newBalance, err := ftm.GetBalance(address, currBlockNum)
 		if err != nil {
 			return fmt.Errorf("unable to get balance for block %s: %s", prevBlockNum.String(), err)
 		}
-		v.cache.StoreBalanceIntoCache(address, newBalance)
-		computedBalance := new(big.Int).Add(oldBalance, amount)
+		newBalances[address] = newBalance
+
+		// compare balance computed from the block internal txs and the old balance with the new balance
+		computedBalance := new(big.Int).Add(oldBalance, balanceChange)
 		if computedBalance.Cmp(newBalance) != 0 {
 			computedDiff := new(big.Int).Sub(computedBalance, oldBalance)
 			realDiff := new(big.Int).Sub(newBalance, oldBalance)
@@ -70,6 +90,11 @@ func (v *Verifier) VerifyBlock(blockNum int64, ftm *client.FtmBridge) error {
 					" previous: %s\n computed: %s  (diff: %s)\n real:     %s  (diff: %s)",
 				address, blockNum, oldBalance.String(), computedBalance.String(), computedDiff.String(), newBalance.String(), realDiff.String())
 		}
+	}
+
+	// override cached balance only when the block succeed (allow retry)
+	for address, newBalance := range newBalances {
+		v.cache.StoreBalanceIntoCache(address, newBalance)
 	}
 
 	return nil
@@ -88,10 +113,10 @@ func (v *Verifier) printTxs(etxs []rpctypes.ExternalTx) {
 	}
 }
 
-func (v *Verifier) printMovements(movements *BlockMovements) {
-	v.printf("Block movements:")
-	for _, address := range movements.Addresses {
-		amount := movements.Map[address]
+func (v *Verifier) printBalancesChanges(changes *BalancesChanges) {
+	v.printf("Block balances changes:")
+	for _, address := range changes.Addresses {
+		amount := changes.Map[address]
 		v.printf("    - %s: %s\n", address, amount.String())
 	}
 }
